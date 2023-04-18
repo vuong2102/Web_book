372593d74e1a3bc44863c6c5aaafda4a521fe774
Vuong <vuong2k2002@gmail.com>
2023-04-18 16:35:31 +0700

    first commit

---
+++ src/main/java/com/example/btl_web_book/connection/JDBCConnect.java
@@ -0,0 +1,17 @@
+package com.example.btl_web_book.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnect {
+    private static Connection connection = null;
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        if(connection == null){
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection= DriverManager.getConnection("jdbc:mysql://localhost:3309/web_book","root","21022002");
+            System.out.print("connected");
+        }
+        return connection;
+    }
+}
